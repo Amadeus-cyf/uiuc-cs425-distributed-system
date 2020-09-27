84becb74b7ea2bcb33cb1839c1720441e102c0e0
enyi <enyij2@illinois.edu>
2020-09-27 20:59:40 +0800

    add sh

---
+++ src/run.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# Define some constants
+#ONSSERVER=mp1Out
+# # PROJECT_PATH= 
+# JAR_PATH=../$PROJECT_PATH
+# BIN_PATH=$PROJECT_PATH/bin
+# SRC_PATH= mp1
+
+# First remove the sources.list file if it exists and then create the sources file of the project
+rm -f mp1/sources
+find mp1 -name *.java > mp1/sources.list
+chmod 777 mp1/sources.list
+ 
+# Compile the project
+javac -d ../out -classpath ../json-20140107.jar @mp1/sources.list
+
+# Run the project
+java -classpath ../out:../json-20140107.jar mp1.Server
+
