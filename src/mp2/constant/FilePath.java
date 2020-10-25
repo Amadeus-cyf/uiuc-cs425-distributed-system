07b180c0421c873b04f9d0f1efdea641202bb39e
Amadeus-cyf <amadeuscyf@gmail.com>
2020-10-25 15:13:19 +0800

    fix root path

--- src/mp2/constant/FilePath.java
+++ src/mp2/constant/FilePath.java
@@ -1,7 +1,7 @@
 package mp2.constant;
 
 public class FilePath {
-    public static final String ROOT = "/home/" + UserInfo.username + "/";
+    public static final String ROOT = "/home/" + UserInfo.username + "/" + "uiuc-cs425-distributed-system/";
     public static final String SDFS_ROOT_DIRECTORY = "sdfs/";
     public static final String LOCAL_ROOT_DIRECTORY = "local/";
 }
