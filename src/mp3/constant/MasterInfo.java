f79f53944938da1232e11d469a49e555a40e80b0
Jiang <enyij2@fa20-cs425-g53-10.cs.illinois.edu>
2020-11-27 23:11:42 -0600

    modify constant

--- src/mp3/constant/MasterInfo.java
+++ src/mp3/constant/MasterInfo.java
@@ -1,8 +1,8 @@
 package mp3.constant;
 
 public class MasterInfo {
-    public static final String Master_IP_ADDRESS = "localhost";
+    public static final String Master_IP_ADDRESS = "fa20-cs425-g53-01.cs.illinois.edu";
     public static final int MASTER_PORT = 3000;
-    public static final String MASTER_FD_IP_ADDRESS = "localhost";
+    public static final String MASTER_FD_IP_ADDRESS = "fa20-cs425-g53-01.cs.illinois.edu";
     public static final int MASTER_FD_PORT = 3001;
 }
--- src/mp3/constant/ServerInfoConst.java
+++
@@ -1,7 +0,0 @@
-package mp3.constant;
-
-public class ServerInfoConst {
-    public static final int NUM_SERVERS = 6;
-    public static final String SERVER_ROOT_IP = "localhost";
-    public static final int SERVER_PORT = 3000;
-}
